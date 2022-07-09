b5c99e373a554d0b499baaf0795f1829008c629b
Juan Rojas <juan.rojas-castr@mail.escuelaing.edu.co>
2022-07-09 16:51:34 -0500

    Create java method

--- java-springboot-src/method/src/main/java/com/dataScienceAnalyst/method/MethodApplication.java
+++ java-springboot-src/method/src/main/java/com/dataScienceAnalyst/method/MethodApplication.java
@@ -1,13 +1,88 @@
 package com.dataScienceAnalyst.method;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfWriter;
+
+@Service
 @SpringBootApplication
 public class MethodApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MethodApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(MethodApplication.class, args);
-	}
+    public void export(HttpServletResponse response) throws IOException {
+
+        DefaultCategoryDataset data = new DefaultCategoryDataset();
+
+        data.setValue(4, "Prueba 1", "Player 1");
+        data.setValue(4, "Prueba 1", "Player 2");
+        data.setValue(5, "Prueba 1", "Player 3");
+
+        data.setValue(2, "Prueba 2", "Player 1");
+        data.setValue(3, "Prueba 2", "Player 2");
+        data.setValue(3, "Prueba 2", "Player 3");
+
+        JFreeChart graphicBar = ChartFactory.createBarChart3D(
+                "Datos",
+                "Titulo abajo",
+                "Puntaje",
+                data,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        BufferedImage objBufferedImage= graphicBar.createBufferedImage(500,300);
+        ByteArrayOutputStream bas = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(objBufferedImage, "png", bas);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        byte[] byteArray=bas.toByteArray();
+
+        InputStream in = new ByteArrayInputStream(byteArray);
+        BufferedImage image = ImageIO.read(in);
+        File outputfile = new File("image.png");
+        ImageIO.write(image, "png", outputfile);
+
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        document.open();
+        Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        fontTitle.setSize(18);
+
+        Paragraph paragraph = new Paragraph("Graficos de los resultados obtenidos", fontTitle);
+        paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+
+        Font fontParagraph = FontFactory.getFont(FontFactory.HELVETICA);
+        fontParagraph.setSize(12);
+
+        Paragraph paragraph2 = new Paragraph("A continuacion se presenta una grafica de barras con los resultados obtenidos en la prueba", fontParagraph);
+        paragraph2.setAlignment(Paragraph.ALIGN_LEFT);
+
+        Image png = Image.getInstance("image.png");
+
+        document.add(paragraph);
+        document.add(paragraph2);
+        document.add(png);
+        document.close();
+    }
 
 }
--- java-springboot-src/method/src/main/java/com/dataScienceAnalyst/method/controller/MainController.java
+++ java-springboot-src/method/src/main/java/com/dataScienceAnalyst/method/controller/MainController.java
@@ -3,15 +3,34 @@ package com.dataScienceAnalyst.method.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.dataScienceAnalyst.method.MethodApplication;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/demo")
 public class MainController {
+    private final MethodApplication methodService;
+
+    public MainController(MethodApplication methodService) {
+        this.methodService = methodService;
+    }
+
     @RequestMapping(value="/test", method = RequestMethod.GET)
-    public String getGraphic(HttpServletRequest request){
-        String graphic = "{name: grafica, descriptio: simulando un JSON}";
-        return  graphic;
+    public void getGraphic(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        this.methodService.export(response);
     }
 }
