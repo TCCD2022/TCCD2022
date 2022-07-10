88224970827894e71930eeff9a19ad0d3605f869
ZulyVargas <zuly.vargas@mail.escuelaing.edu.co>
2022-07-10 15:59:15 -0500

    Col_ids correction

--- Pythonsrc/linearRegression.py
+++ Pythonsrc/linearRegression.py
@@ -9,8 +9,8 @@ import json
 from datetime import datetime
 """
  This method returns the plot corresponding to the linear
- regression of the first two columns indicated in the received parameter
- Parameters:
+ rregression of the first two columns indicated in the received parameter
+ Paraameters:
  --------------------------------------------
  data: Object with the necessary information to construct the plot: file name, 
        column names, plot title and color of the linear regression line.
@@ -20,16 +20,32 @@ def linearRegressionMethod(data):
     columns = data["col_ids"][:2];
     x = []
     y = []
-    indexX = int(columns[0]["colid"])
-    indexY = int(columns[1]["colid"])
+    indexX = ""
+    indexY = ""
+    nameX = columns[0]["colname"].replace('"','')
+    nameY = columns[1]["colname"].replace('"','')
+    #print("Name X ", nameX)
+    #print("Name Y ", nameY)
     # open file
     with open("/code/media/"+data["filename"], "r") as my_file:
     # pass the file object to reader()
         file_reader = reader(my_file)
-        next(file_reader,None)
+        head =  next(file_reader,None)
+        #head =  head[0]
+        print("HEAD ", head)
+        for i in range(0,len(head)):
+            print("HEAD i ", head[i])
+            if (head[i].replace('"','') == nameX ):
+                indexX= i
+                #print("If x ..", str(indexX))
+            if (head[i].replace('"','') == nameY):
+                indexY = i
+                #print("If y...", str(indexY))
         # do this for all the rows
         for i in file_reader:
-            array =  i[0].split(",")
+            #print("i..."+ str(i))
+            array =  i
+            #print("Array..."+ str(array))
             x.append(float(array[indexX]))
             y.append(float(array[indexY]))
     colorPlot = data["colour"]
@@ -48,6 +64,7 @@ def linearRegressionMethod(data):
     #Plot
     sns.regplot(x, y,color=colorPlot).set(title=titlePlot)
     plt.savefig(fileName)
+    plt.clf()
     print("linearRegression....",fileName)
     return {"pdffile":[fileName], "format":["pdf"]}
 
