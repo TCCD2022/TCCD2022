d39f4dfe8965620ce6a4a10f1f47c87a649daef7
Daniel19902 <zoe19902@gmail.com>
2022-07-12 21:16:22 -0500

    empalme para pull request

--- Pythonsrc/app.py
+++ Pythonsrc/app.py
@@ -1,10 +1,35 @@
 import json
-
+from flask import Flask, jsonify
+from utils import *
+from flask import request
 from flask import *
 from relationPlots import *
 
 
 app = Flask(__name__)
+
+"""
+    post: /lr
+    Projection of the linear regression of two columns of numbers.
+"""
+@app.route('/lr',methods = ['POST'])
+def linearRegression():
+    values =  json.loads(request.form.getlist("metadata")[0])
+    result = linearRegressionMethod(values)
+    return result
+
+@app.route('/corr',methods = ['POST'])
+def correlation_plot():
+    values =  json.loads(request.form.getlist("metadata")[0])
+    result = correlation_plot_method(values)
+    return result
+
+"""
+    POST:/rp
+    rp relation plots
+
+"""
+
 
 @app.route('/rp',methods = ['POST'])
 
