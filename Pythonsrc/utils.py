b9e7b8958bde5f102a8a127413bb7b1c0239294e
nontoa <nicolasnc010@gmail.com>
2022-07-13 23:23:56 -0500

    Implementing method and changing file name

--- Pythonsrc/utils.py
+++ Pythonsrc/utils.py
@@ -127,6 +127,34 @@ def bar_chart_plot_method(data):
     print("Bar Chart Plot....",name)
     return {"pdffile":[name], "format":["pdf"]}
 
+def nnc_bar_graphic_method(data):
+    '''
+    This method returns a bar graphic with a comparission of the first variable with 2
+    different categories.
+    Parameters:
+        data: Object with the necessary information to construct the graphic: Graphic title, 
+        column names, and dimensions for the graphic. 
+    '''
+    columns = [c['colname'] for c in data['col_ids']]
+    readFile = pd.read_csv('/code/media/'+data["filename"], usecols=columns)
+    df = readFile [columns]
+    graph = df.groupby(columns[0])[columns[1], columns[2]].mean()
+    graph.plot.barh()
+    plt.title(data["title"])
+    figure = plt.gcf()
+    figure.set_size_inches(data["width"]*0.39,data["height"]*0.39)
+    tmp =  data["filename"].split("/")[:-1]
+    cpath = "/".join(tmp)
+    nameFile = data["filename"].split("/").pop()
+    path = '/code/media/'+ cpath +'/dvg--results-/'+ nameFile + "/"
+    name = path + data["title"]+".pdf"
+    if (os.path.exists(path) == False):
+    	pathDir = Path(path)
+    	pathDir.mkdir(parents=True) 
+    plt.savefig(name, format = "pdf")
+    print("Bar Graphic....",name)
+    return {"pdffile":[name], "format":["pdf"]}
+
 def get_filename(data_filename,user_filename):
     tmp =  data_filename.split("/")[:-1]
     currentPath = "/".join(tmp)
