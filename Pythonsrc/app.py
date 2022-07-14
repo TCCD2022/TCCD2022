56b345d3549345534849bdeec167cb9ba095a62a
nontoa <nicolasnc010@gmail.com>
2022-07-13 19:05:51 -0500

    Updating docker-compose and removing unnecessary folder

--- Pythonsrc/app.py
+++ Pythonsrc/app.py
@@ -29,6 +29,12 @@ def bar_chart_plot():
     result = bar_chart_plot_method(values)
     return result
 
+@app.route('/nncBarGraphic',methods = ['POST'])
+def nnc_bar_graphic():
+    values =  json.loads(request.form.getlist("metadata")[0])
+    result = nnc_bar_graphic_method(values)
+    return result
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5001,debug=True)
