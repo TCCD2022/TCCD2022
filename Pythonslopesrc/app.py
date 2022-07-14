16125ac1a1fffd96a1ff2c696a2b35548102a758
BrayhanVargas <balbervq@gmail.com>
2022-07-14 11:06:15 -0500

    add slope_method and show graph in front

--- Pythonslopesrc/app.py
+++ Pythonslopesrc/app.py
@@ -1,21 +1,66 @@
+import json
 from flask import Flask, jsonify
 from flask import request
+from matplotlib import pyplot as plt
+from pathlib import Path
+import os
 import json
-from slopeService import slope_method
+from datetime import datetime
+import pandas as pd
+#from Pythonslopesrc.slopeService import slope_method
 
 
 app = Flask(__name__)
 
 
-@app.route('/',methods = ['GET'])
+@app.route('/', methods=['GET'])
 def helloslope():
-    print("Holaaaaa///")
+    print("Test API OK")
     return "helloWorld"
-    
-@app.route('/slope',methods = ['POST'])
+
+
+@app.route('/slope', methods=['POST'])
 def slope():
-    # result = slope_method()
-    return {"pdffile":["HolaMundo"], "format":["pdf"]}
+    values = json.loads(request.form.getlist("metadata")[0])
+    file_name = values["filename"]
+    cols = [col['colname'] for col in values['col_ids']]
+    df = pd.read_csv('/code/media/'+file_name, usecols=cols)
+
+    x1 = df[cols[0]][0]
+    x2 = df[cols[0]][1]
+
+    y1 = df[cols[1]][0]
+    y2 = df[cols[1]][1]
+
+    m = (y2-y1)/(x2-x1)
+    b = y1 - m*x1
+
+    plt.plot(df[cols[0]], df[cols[1]])
+    print(m)
+    print(b)
+
+    tmp = file_name.split("/")[:-1]
+    current_path = "/".join(tmp)
+    # Take the file name without extension
+    current_name_file = file_name.split("/")[-1].split('.')[0]
+
+    date = str(datetime.now())
+    path_name = '/code/media/' + current_path + \
+        'dvg--results-/' + current_name_file + "/"
+    file_name = path_name + values["title"] + date + ".pdf"
+
+    if (os.path.exists(path_name) == False):
+        path = Path(path_name)
+        path.mkdir(parents=True)
+
+    plt.savefig(file_name)
+    plt.clf()
+    print("file_name...", file_name)
+
+    # result = slope_method(values)
+    # return result
+    return {"pdffile": [file_name], "format": ["pdf"]}
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8080,debug=True) 
+    app.run(host="0.0.0.0", port=8080, debug=True)
--- Pythonslopesrc/slopeService.py
+++ Pythonslopesrc/slopeService.py
@@ -1,2 +1,45 @@
-def slope_method():
-     return {"pdffile":["HolaMundo"], "format":["pdf"]}
+from matplotlib import pyplot as plt
+from pathlib import Path
+import os
+import json
+from datetime import datetime
+import pandas as pd
+
+
+def slope_method(values):
+    file_name = values["filename"]
+    cols = [col['colname'] for col in values['col_ids']]
+    df = pd.read_csv('/code/media/'+file_name, usecols=cols)
+
+    x1 = df[cols[0]][0]
+    x2 = df[cols[0]][1]
+
+    y1 = df[cols[1]][0]
+    y2 = df[cols[1]][1]
+
+    m = (y2-y1)/(x2-x1)
+    b = y1 - m*x1
+
+    plt.plot(df[cols[0]], df[cols[1]])
+    print(m)
+    print(b)
+
+    tmp = file_name.split("/")[:-1]
+    current_path = "/".join(tmp)
+    # Take the file name without extension
+    current_name_file = file_name.split("/")[-1].split('.')[0]
+
+    date = str(datetime.now())
+    path_name = '/code/media/' + current_path + \
+        'dvg--results-/' + current_name_file + "/"
+    file_name = path_name + values["title"] + date + ".pdf"
+
+    if (os.path.exists(path_name) == False):
+        path = Path(path_name)
+        path.mkdir(parents=True)
+
+    plt.savefig(file_name)
+    plt.clf()
+    print("file_name...", file_name)
+
+    return {"pdffile": [file_name], "format": ["pdf"]}
